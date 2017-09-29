36fdf191397c063b90868407ebbc9d395a5a1d65
Juan Volpintesta <jvolpintesta@gmail.com>
2017-09-29 04:02:03 +0100

    esa

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -53,6 +53,11 @@ public class Main {
     return "index";
   }
 
+  @RequestMapping("/write")
+  String write() {
+    return "write";
+  }
+
   @RequestMapping("/db")
   String db(Map<String, Object> model) {
     try (Connection connection = dataSource.getConnection()) {
